7cd2b5ddfbc3b7fc188ba656aaf2f07e99cfc057
Oscar Poch <opoch@seuba.com>
2021-06-16 15:16:09 +0200

    Add project files.

---
+++ NORTUA_ImageResize/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NORTUA_ImageResize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
